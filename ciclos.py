d018aef614dd8cdfc2832f48d406b8a3ea7aad9d
leonardoernesto <145036285+leonardoernesto@users.noreply.github.com>
2023-11-09 00:46:27 -0600

    Create ciclos.py

---
+++ ciclos.py
@@ -0,0 +1,34 @@
+print("presiona enter para avanzar")
+input()
+print("Empezemos a conatar del 1 al 20, pero solo los pares")
+x=input()
+while x=="":
+  print(1)
+  y=input()
+  if y=="alto":
+    print("entendi hasta aqui")
+    x="alto"
+  else:
+    print(2)
+    y=input()
+    if y=="alto":
+      print("entendi hasta aqui")
+      x="alto"
+    else:
+      print(3)
+      y=input()
+      if y== "alto":
+        print("entendi hasta aqui")
+        x="alto"
+      else:
+        print(4)
+        y=input()
+        if y== "alto":
+          print("entendi hasta aqui")
+          x="alto"
+        else:
+          print(5)
+          y=input()
+          if y== "alto":
+            print("entendi hasta aqui")
+            x="alto"
