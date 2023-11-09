bc20774cdcf0301d0c016246dd433b1db79343ef
leonardoernesto <145036285+leonardoernesto@users.noreply.github.com>
2023-11-09 00:44:56 -0600

    Create retroalimentacion.py

---
+++ retroalimentacion.py
@@ -0,0 +1,12 @@
+"""
+En esta aventura seras un capitan que tndra que llevar un equipo de soldados a un planeta en ataque tendras que tomar decisiones para completar exitosamente tu mision pero ten cuidado pues quien ataca el planeta no es humano y mucho menos amigable
+"""
+print("buenos dias capitan, soy Mon la nueva asistente virtual de tu fragata")
+print("Esta nave es la DOG de la UNSC")
+print("Es 26 de Julio de 2556, hemos recibido nuevas ordenes de transporte")
+print("Tenemos que lleva al equipo de spartans venus al planeta en ataque Reach")
+print("primero tendremos que crear un plan para cruzar el espacio hostil")
+print("¿a) Partimos ahora o b) cargamos recursos para el viaje")
+x=input(":"  )
+print("  ")
+print("")
